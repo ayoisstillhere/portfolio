fac9507033e4ec962720bcb940a74fe1366e6805
Ayodele Fagbami <afagbami@gmail.com>
2023-08-18 15:51:29 +0100

    added typing effect

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,50 @@
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 
 const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = [
+    "Software Engineer",
+    "Mobile App Developer",
+    "Web Developer",
+  ];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta/2);
+        }
+
+        if (!isDeleting && updatedText == fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -10,8 +52,8 @@ const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to My Portfolio</span>
             <h1>
-              {`Hi I'm Ayodele`}
-              <span className="wrap">software engineer</span>
+              {`Hi I'm Ayodele, a `}
+              <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -21,7 +63,7 @@ const Banner = () => {
             </p>
             <button onClick={() => console.log("Connect")}>
               Let's Connect
-              <ArrowRightCircle size={25}/>
+              <ArrowRightCircle size={25} />
             </button>
           </Col>
           <Col xs={12} md={6} xl={5}>
