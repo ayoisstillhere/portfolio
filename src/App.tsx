2f9b59f20e75317ef15dec9af2b14ef5c9f7e59b
Ayodele Fagbami <afagbami@gmail.com>
2023-08-18 15:06:15 +0100

    added navbar to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <NavBar />
+  </div>;
 }
 
 export default App;
